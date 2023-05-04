77284c030c9f8c9e2aa5cd24aeece2c86007f511
Nikita <7masteryakali@gmail.com>
2023-05-04 11:19:05 +0300

    feat: 03-files-in-folder done

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,19 @@
+const fsPromises = require('fs/promises');
+const path = require('node:path'); 
+const readline = require('readline');
+
+async function filesInFolder(folderName){
+  try {
+    const folder = await fsPromises.readdir(path.join(folderName),{withFileTypes: true});
+    for (const file of folder) {
+      if (file.isFile()) {
+        console.log(file);
+      } else if (file.isDirectory()) {
+       filesInFolder(`${folderName}/${file.name}`);
+      }
+    }
+  } catch (err) {
+    console.error(err);
+  }
+}
+filesInFolder('secret-folder');
