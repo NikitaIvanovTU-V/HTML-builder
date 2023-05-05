8821efb83aa99d4e4375e02bd94cde5d6025b784
Nikita <7masteryakali@gmail.com>
2023-05-05 10:37:48 +0300

    feat: make 05-merge-styles function exportable

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -4,11 +4,11 @@ const path = require('node:path');
 
 const curFolder = '05-merge-styles';
 
-async function mergeStyles() {
+exports.mergeStyles = async function mergeStyles(curFolder,mergedFile) {
   try {
     const folder = await fsPromises.readdir(path.join(`${curFolder}/styles`),
     {withFileTypes: true});
-    const bundle = fs.createWriteStream(`${curFolder}/project-dist/bundle.css`)
+    const bundle = fs.createWriteStream(`${curFolder}/${mergedFile}`)
     for (const file of folder) {
       if(file.isFile() && path.extname(file.name) === '.css') {
         const readFile = fs.createReadStream(`${curFolder}/styles/${file.name}`);
@@ -20,4 +20,4 @@ async function mergeStyles() {
     console.error(err.message);
   }
 }
-mergeStyles();
+exports.mergeStyles(curFolder,'project-dist/bundle.css');
