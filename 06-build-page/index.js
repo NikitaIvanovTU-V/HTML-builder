c2054f2bc1165e4a3bb365649470a4eabaa4ddc3
Nikita <7masteryakali@gmail.com>
2023-05-05 10:39:46 +0300

    feat: add function to change template tags to factual components

---
+++ 06-build-page/index.js
@@ -0,0 +1,30 @@
+const fsPromises = require('fs/promises');
+const fs = require('fs');
+const path = require('node:path');
+const curFolder = '06-build-page';
+
+async function buildPage() {
+  try {
+    await fsPromises.mkdir(path.join(`${curFolder}/project-dist`), { recursive: true });
+    function fileToString(stream) {
+      const chunks = [];
+      return new Promise((resolve, reject) => {
+        stream.on('data', (chunk) => chunks.push(Buffer.from(chunk)));
+        stream.on('error', (error) => reject(error));
+        stream.on('end', () => resolve(Buffer.concat(chunks).toString()));
+      })
+    };
+
+    let templateData = await fileToString(fs.createReadStream(`${curFolder}/template.html`));
+    const components = await fsPromises.readdir(path.join(`${curFolder}/components`));
+    for (const file of components) {
+      const fileData = await fileToString(fs.createReadStream(`${curFolder}/components/${file}`));
+      templateData = templateData.replace(`{{${path.basename(file, '.html')}}}`, fileData);
+    }
+    const result = fs.createWriteStream(`${curFolder}/project-dist/index.html`);
+    result.write(templateData);
+  } catch (err) {
+    console.error(err.message);
+  }
+}
+buildPage();
