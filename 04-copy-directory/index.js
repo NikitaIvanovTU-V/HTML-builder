dfd013b250f750cff42c6a8b57b1ca9c233b4858
Nikita <7masteryakali@gmail.com>
2023-05-05 10:47:18 +0300

    feat: make 04-copy-directory function exportable

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -2,16 +2,16 @@ const fsPromises = require('fs/promises');
 const path = require('node:path'); 
 
 const curFolder = '04-copy-directory';
-async function copyDir() {
+exports.copyDir = async function copyDir(curFolder,folderToCopy,whereToCopy) {
   try {
-    await fsPromises.mkdir(`${curFolder}/files-copy`,{ recursive: true });
-    const folder = await fsPromises.readdir(path.join(`${curFolder}/files`));
+    await fsPromises.mkdir(`${curFolder}/${whereToCopy}`,{ recursive: true });
+    const folder = await fsPromises.readdir(path.join(`${curFolder}/${folderToCopy}`));
     for (const file of folder) {
-      await fsPromises.copyFile(`${curFolder}/files/${file}`,
-      `${curFolder}/files-copy/${file}`);
+      await fsPromises.copyFile(`${curFolder}/${folderToCopy}/${file}`,
+      `${curFolder}/${whereToCopy}/${file}`);
     }
   } catch (err) {
     console.error(err.message);
   }
 }
-copyDir();
+exports.copyDir(curFolder,'files','files-copy');
